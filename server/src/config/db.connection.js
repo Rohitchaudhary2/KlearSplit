6c03e5dee859fe44f7de2c4c79d0591e7796a883
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-26 09:47:16 +0530

    Testing Husky

--- server/src/config/db.connection.js
+++ server/src/config/db.connection.js
@@ -10,11 +10,8 @@ const sequelize = new Sequelize(database, username, password, {
   logging: false,
 });
 
-try {
-  await sequelize.authenticate();   // Attempting to authenticate the connection to the database
-  logger.log({level: "info", statusCode: 200, message: "Connection has been established successfully."})
-} catch {
-  logger.log({
+try {await sequelize.authenticate();   // Attempting to authenticate the connection to the database
+  logger.log({level: "info", statusCode: 200, message: "Connection has been established successfully."})} catch {logger.log({
     level: "error",
     statusCode: 503,
     message: 'Service unavailable. Unable to connect to the database.',

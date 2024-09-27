aeefcaa4ded30cb7ddc0cae6375439fb20ecd05f
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-27 09:29:52 +0530

    Modified Otp handling logic

--- server/src/api/v1/users/otpDb.js
+++ server/src/api/v1/users/otpDb.js
@@ -2,10 +2,11 @@ import Otp from "./models/otpModel.js";
 
 export const createOtpDb = async (otp) => await Otp.create(otp);
 
-export const getOtpDb = async (email, phone) =>
+export const getOtpDb = async (email, phone, otp) =>
   await Otp.findOne({
     where: {
       email,
       phone,
+      otp,
     },
   });

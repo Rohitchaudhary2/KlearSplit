a31d2f2fc48efd1fb0d04839f9205e71a0ac4a67
Deepanshu Dixit <107026009+hackbizz@users.noreply.github.com>
2024-11-24 13:16:37 +0530

    Minor fix for importing crypto

--- server/src/api/middlewares/uploadMiddleware.js
+++ server/src/api/middlewares/uploadMiddleware.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import crypto from "crypto";
 import { ErrorHandler } from "./errorHandler.js";
 
 const __dirname = path.resolve();
